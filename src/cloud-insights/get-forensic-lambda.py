27c3cd4aeabed35eceb5895a95deb1eb277532c4
mottai <konakona191@gmail.com>
2021-06-23 03:31:43 +0000

    update Get Search Range Automation

--- src/cloud-insights/get-forensic-lambda.py
+++ src/cloud-insights/get-forensic-lambda.py
@@ -2,8 +2,8 @@ import os
 from urllib import request, parse
 import json
 from pprint import pprint
-import datetime
-import time
+import datetime, time, calendar
+from dateutil.relativedelta import relativedelta
 import pandas
 import boto3
 
@@ -11,17 +11,20 @@ import boto3
 url = os.environ['CLOUD_SECURE_URL']
 token = os.environ['CLOUD_SECURE_TOKEN']
 svm = os.environ['GET_DEVICE_NAME']
-from_date = '20210601'
-to_date = '20210630'
 output_path = '/tmp/'
-output_file = from_date + '_' + svm + '_' + 'output.csv'
 upload_bucket = 'myuichi-python-upload-test-bucket'
 
-def conv_epochtime(date):
-	date_time = datetime.datetime.strptime(date, '%Y%m%d')
-	date_time = date_time.astimezone(datetime.timezone(datetime.timedelta(hours=-9)))
-	epochtime = int(time.mktime(date_time.timetuple()) * 1000)
-	return epochtime
+### 検索レンジ生成 ###
+def get_search_time(date):
+	last = date - relativedelta(months=1)
+	begin_date = datetime.datetime(last.year, last.month, 1)
+	begin_date = int(time.mktime(begin_date.timetuple()) * 1000)
+	end_date = datetime.datetime(date.year, date.month, 1)
+	end_date = int(time.mktime(end_date.timetuple()) * 1000)
+	### 月末日の取得 ###
+	# last_end_date = datetime.datetime(last.year, last.month, calendar.monthrange(last.year, last.month)[1])
+	# last_end_date = int(time.mktime(last_end_date.timetuple()) * 1000)
+	return begin_date, end_date
 
 def lambda_handler(event, context):
 	### S3 オブジェクト情報取得 ###
@@ -29,12 +32,12 @@ def lambda_handler(event, context):
 	bucket = s3.Bucket(upload_bucket)
 
 	### Get Request クエリストリング & ヘッダ 定義 ###
-	from_epochtime = conv_epochtime(from_date)
-	to_epochtime = conv_epochtime(to_date)
+	today = datetime.datetime.utcnow()
+	begin_date, end_date = get_search_time(today)
 	param = {
 		'deviceName': svm,
-		'fromTime': from_epochtime,
-		'toTime': to_epochtime
+		'fromTime': begin_date,
+		'toTime': end_date
 	}
 	headers = {
 		'x-cloudinsights-apikey': token
@@ -49,6 +52,7 @@ def lambda_handler(event, context):
 	with request.urlopen(req) as res:
 		body = json.loads(res.read())
 		output_body = pandas.json_normalize(body['results'])
+		output_file = str(today.year) + str(today.month) + '_' + svm + '_' + 'output.csv'
 		output_body.to_csv(output_path + output_file, encoding='utf-8')
 		bucket.upload_file(output_path + output_file, output_file)
 
